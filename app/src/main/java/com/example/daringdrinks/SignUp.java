af80cee93d63456dc7567f42848561a2be3df07a
edwinaringo <tedaringo628@gmail.com>
2021-07-28 12:08:36 +0300

    Created validation for the Sign Up activity

--- app/src/main/java/com/example/daringdrinks/SignUp.java
+++ app/src/main/java/com/example/daringdrinks/SignUp.java
@@ -3,12 +3,76 @@ package com.example.daringdrinks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SignUp extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private EditText editTextFullName, editTextEmail, editTextPassword;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signUp = (Button) findViewById(R.id.signUp);
+        signUp.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.fullName);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.signUp:
+                signUp();
+                break;
+        }
+    }
+
+    private void signUp() {
+        String email = editTextEmail.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (fullName.isEmpty()) {
+            editTextFullName.setError("Full name is required");
+            editTextFullName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length()< 6) {
+            editTextPassword.setError("Minimum password length is 6 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
+
     }
 }
