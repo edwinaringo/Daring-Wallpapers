0fa9a4dd3b88c37840fd88ca81dc793be15d5e3b
edwinaringo <tedaringo628@gmail.com>
2021-07-29 15:56:03 +0300

    Finished up on the adapter class for the wallPaper adapter

--- app/src/main/java/com/example/daringdrinks/WallpaperRVAdapter.java
+++ app/src/main/java/com/example/daringdrinks/WallpaperRVAdapter.java
@@ -1,4 +1,54 @@
 package com.example.daringdrinks;
 
-public class WallpaperRVAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class WallpaperRVAdapter extends RecyclerView.Adapter<WallpaperRVAdapter.ViewHolder> {
+    private ArrayList<String> wallpaperRVArrayList;
+    private Context context;
+    @NonNull
+    @Override
+    public WallpaperRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.wallpaper_rv_item,parent,false);
+        return new WallpaperRVAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  WallpaperRVAdapter.ViewHolder holder, int position) {
+        Glide.with(context).load(wallpaperRVArrayList.get(position)).into(holder.wallpaperIV);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context,SetWallpaperActivity.class);
+                i.putExtra("imgUrl",wallpaperRVArrayList.get(position));
+                context.startActivity(i);
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return wallpaperRVArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView wallpaperIV;
+        public ViewHolder(@NonNull  View itemView) {
+            super(itemView);
+            wallpaperIV = itemView.findViewById(R.id.idIVWallpaper);
+        }
+    }
 }
