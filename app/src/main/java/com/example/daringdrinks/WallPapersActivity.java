aee5a674e4d0d3e9a662e69fb0afac3be7654408
edwinaringo <tedaringo628@gmail.com>
2021-07-29 14:49:37 +0300

    Created a model class for the recycler view

--- app/src/main/java/com/example/daringdrinks/WallPapersActivity.java
+++ app/src/main/java/com/example/daringdrinks/WallPapersActivity.java
@@ -1,14 +1,29 @@
 package com.example.daringdrinks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class WallPapersActivity extends AppCompatActivity {
+
+    private EditText searchEdit;
+    private ImageView searchIV;
+    private RecyclerView categoryRV,wallpaperRV;
+    private ProgressBar loadingPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallpapers);
+
+        searchEdit = findViewById(R.id.idEditSearch);
+        searchIV = findViewById(R.id.idIVSearch);
+        categoryRV = findViewById(R.id.idRVCategory);
+        wallpaperRV = findViewById(R.id.iRVWallpapers);
+        loadingPB = findViewById(R.id.idPBLoading);
     }
 }
